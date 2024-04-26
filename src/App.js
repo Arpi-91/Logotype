be84fb132f167491f085572d344c8a3134c23643
Arpi Arakelyan <arpinearakelyan01@gmail.com>
2024-04-27 01:59:58 +0400

    add logotype project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import './App.css';
-import Header from './components/header/Header';
-
-
-
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Header from "./components/header/Header";
+import { useEffect, useState } from "react";
 
 function App() {
-  let myStates = useSelector((state) => state)
-  
+  let myStates = useSelector((state) => state);
+
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      return myStates;
+      // return data
+    };
+    fetchData().then((data) => setDatas(data));
+  }, [myStates]);
+  console.log(datas);
 
   return (
     <div className="App">
       <Header />
 
-      <div className='container'>
-        {
-          myStates.items.map(elem =>{
-            return <div id={elem.id} className='container_item'>
-              <h3>{elem.categories}</h3>              
-              <img src={require(`${elem.src}`)} alt="photo" />
-              <h1>{elem.title}</h1>
-              <div className="veiw_section">
-                <span>{elem.author}</span>
-                <h5>{elem.data}</h5>
-                <h5>{elem.views}</h5>
+      <div className="container">
+        {datas &&
+          datas.filteredDatas.map((elem) => {
+            return (
+              <div id={elem.id} className="container_item">
+                <h3>{elem.categories}</h3>{" "}
+                <img src={require(`${elem.src}`)} alt="photo" />
+                <h1>{elem.title}</h1>
+                <div className="veiw_section">
+                  <span>{elem.author}</span>
+                  <h5>{elem.data}</h5>
+                  <h5>{elem.views}</h5>
+                </div>
+                <p>{elem.content}</p>
               </div>
-              <p>{elem.content}</p>
-            </div>
-          })
-        }
+            );
+          })}
       </div>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,120 +1,173 @@
+import axios from "axios";
 import { createStore } from "redux";
+// import initialstate from '../App'
+// const initialState = {
+//     items:[{   
+//         "src": "./assets/images/Rectangle_1.png",
+//         "id": 1,
+//         "categories": "Lifestyle",
+//         "title":"Eat Right For Your Exercise Regime",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {  
+//         "src": "./assets/images/Rectangle_2.png",
+//         "id": 2,
+//         "categories": "Lifestyle",
+//         "title":"The Look: Perfect Balance",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {   
+//         "src": "./assets/images/Rectangle_3.png",
+//         "id": 3,
+//         "categories": "Style",
+//         "title":"Fun Things to Do in Rome",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_4.png",
+//         "id": 4,
+//         "categories": "Style",
+//         "title":"24 Colorful Ceilings That Add Unexpected Contrast to Any Room",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_5.png",
+//         "id": 5,
+//         "categories": "Lifestyle",
+//         "title":"9 New Songs to Heat Up Your Fall Playlist",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_6.png",
+//         "id": 6,
+//         "categories": "Events",
+//         "title":"What You Need on Your Bedside Table",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_7.png",
+//         "id": 7,
+//         "categories": "Travel",
+//         "title":"When Two Wheels Are Better Than Four",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_8.png",
+//         "id": 8,
+//         "categories": "Travel",
+//         "title":"26 Living Room Ideas from the Homes of Top Designers",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     },
+//     {
+//         "src": "./assets/images/Rectangle_1.png",
+//         "id": 9,
+//         "categories": "Music",
+//         "title":"What Makes Your City’s Style Unique",
+//         "author": "Niek Bove",
+//         "data": "April 8, 2018",
+//         "views":"3K Views",
+//         "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
+//     }]
+// }
+// const initialState = {
+//     states: [],
+//     filteredDatas:[]
+//   }
+const initialState = getStates()
 
-const initialState = {
-    items:[{   
-        "src": "./assets/images/Rectangle_1.png",
-        "id": 1,
-        "categories": "Lifestyle",
-        "title":"Eat Right For Your Exercise Regime",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {  
-        "src": "./assets/images/Rectangle_2.png",
-        "id": 2,
-        "categories": "Lifestyle",
-        "title":"The Look: Perfect Balance",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {   
-        "src": "./assets/images/Rectangle_3.png",
-        "id": 3,
-        "categories": "Style",
-        "title":"Fun Things to Do in Rome",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_4.png",
-        "id": 4,
-        "categories": "Style",
-        "title":"24 Colorful Ceilings That Add Unexpected Contrast to Any Room",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_5.png",
-        "id": 5,
-        "categories": "Lifestyle",
-        "title":"9 New Songs to Heat Up Your Fall Playlist",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_6.png",
-        "id": 6,
-        "categories": "Events",
-        "title":"What You Need on Your Bedside Table",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_7.png",
-        "id": 7,
-        "categories": "Travel",
-        "title":"When Two Wheels Are Better Than Four",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_8.png",
-        "id": 8,
-        "categories": "Travel",
-        "title":"26 Living Room Ideas from the Homes of Top Designers",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    },
-    {
-        "src": "./assets/images/Rectangle_1.png",
-        "id": 9,
-        "categories": "Music",
-        "title":"What Makes Your City’s Style Unique",
-        "author": "Niek Bove",
-        "data": "April 8, 2018",
-        "views":"3K Views",
-        "content":"Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button…"
-    }]
+function getStates(){
+    const state = {
+    states: [],
+    filteredDatas:[]
+  }
+    axios.get('http://localhost:8080/items')
+    .then(data => {
+        state.states = data.data
+        state.filteredDatas = data.data
+    })
+    return state
 }
 
 
-function rootReduser(state=initialState, action){
+function rootReduser(state, action){
 
+    // filteri logikana
     switch(action.type){
-        // filteri logikana
-        case 'ITEM':{
-            const initialFilter = initialState.items.filter((i)=>{
+       
+        case 'FILTER_BY_CATEGORY':{
+            console.log(state.states)
+            const initialFilter = state.states.filter((i)=>{
                 return i.categories === action.payload || action.payload === 'All'
             })
-            return {...state, items:initialFilter }
+            console.log(initialFilter)
+            return {...state, filteredDatas:initialFilter}
         };
-        case 'INPUT_VALUE':{
-            const initialSearch = initialState.items.filter((el=>{
+        case 'FILTER_BY_INPUT_VALUE':{
+            const initialSearch = state.states.filter((el=>{
                 return el.categories.toLocaleLowerCase().includes(action.payload.toLocaleLowerCase())
             }))
-            return {...state, items: initialSearch}
+            return {...state, filteredDatas:initialSearch }   
         };
         default:{
+            // console.log(initialState)
             return state;
         }
     }
 }
 
+// function rootReduser(state, action){
+//     const initialState = {
+//         states: [],
+//         filteredDatas:[]
+//       }
+//     // filteri logikana
+//     switch(action.type){
+//         case 'GET_ALL_DATAS':{
+//             return {...state, filteredDatas: action.payload}
+//         };
+//         case 'FILTER_BY_CATEGORY':{
+//             console.log(state.items)
+//             const initialFilter = state.filteredDatas.filter((i)=>{
+//                 return i.categories === action.payload || action.payload === 'All'
+//             })
+//             console.log(initialFilter)
+//             return {...state, items:initialFilter}
+//         };
+//         case 'FILTER_BY_INPUT_VALUE':{
+//             const initialSearch = initialState[0].data.filter((el=>{
+//                 return el.categories.toLocaleLowerCase().includes(action.payload.toLocaleLowerCase())
+//             }))
+//             return {...state, items:initialSearch  }   
+//         };
+//         default:{
+//             console.log(initialState)
+//             return state;
+//         }
+//     }
+// }
 const store = createStore(rootReduser, initialState)
 export default store;
